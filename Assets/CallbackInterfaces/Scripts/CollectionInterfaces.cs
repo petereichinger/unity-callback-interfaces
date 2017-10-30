0c8c60776966fbddec1ccdd13238b0fcc6fd5f65
Peter Eichinger <peter.eichinger@gmail.com>
2017-10-30 17:08:05 +0100

    Add collection interfaces for 3 common cases

---
+++ Assets/CallbackInterfaces/Scripts/CollectionInterfaces.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CallbackInterfaces.Collections {
+    public interface IStandardLifecycle : IAwake, IStart, IUpdate, IOnDestroy{
+        
+    }
+
+    public interface ICollidable : IOnCollisionEnter, IOnCollisionStay, IOnCollisionExit {
+        
+    }
+
+    public interface ICollidable2D : IOnCollisionEnter2D, IOnCollisionStay2D, IOnCollisionExit2D {
+        
+    }
+}
